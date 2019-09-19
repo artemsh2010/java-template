fe2a2d922de63c41589276187e2e83e9c606cc3f
st068204 <artemsh2010>
2019-09-19 11:26:18 +0300

    sort

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -9,7 +9,36 @@ import java.util.List;
  */
 public class IntSort {
   public static void sort (int array[]) {
-    Arrays.sort(array);
+    int n=array.length;
+    quickSort(array,0,n-1);
+  }
+
+  public static void quickSort(int array[],int left,int right){
+    if(array.length!=0)
+    {
+      int beg=left,end=right,med=array[(left+right)/2];
+      while (beg <= end) {
+        while (array[beg] < med) {
+          beg++;
+        }
+
+        while (array[end] > med) {
+          end--;
+        }
+
+        if (beg <= end) {//меняем местами
+          int temp = array[beg];
+          array[beg] = array[end];
+          array[end] = temp;
+          beg++;
+          end--;
+        }
+      }
+      if(left<end) quickSort(array,left,end);
+      if(beg<right) quickSort(array,beg,right);
+
+
+    }
   }
 
   public static void sort (List<Integer> list) {
